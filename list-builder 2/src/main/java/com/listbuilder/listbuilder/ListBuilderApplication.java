580b6b0f95b38e2cd72db967f096d4580bd1f487
colemankus <hanshi2929@gmail.com>
2023-11-11 15:38:25 -0600

    started it up

---
+++ list-builder 2/src/main/java/com/listbuilder/listbuilder/ListBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.listbuilder.listbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListBuilderApplication.class, args);
+	}
+
+}
---
+++ list-builder 2/src/test/java/com/listbuilder/listbuilder/ListBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listbuilder.listbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
